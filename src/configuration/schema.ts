f268de8cad0961c1be2f9ca9acfc73863431d1bb
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-02-07 03:33:35 -0500

    Add defaults object to configuration schema

--- src/configuration/schema.ts
+++ src/configuration/schema.ts
@@ -2,6 +2,7 @@ import { JSONSchema } from "json-schema-typed"
 
 export const enum Properties {
   containerRuntime = "containerRuntime",
+  defaults = "defaults",
 }
 
 export enum ContainerRuntimes {
@@ -12,9 +13,43 @@ export enum ContainerRuntimes {
 // https://github.com/Microsoft/TypeScript/issues/14106#issuecomment-280253269
 export type SchemaKeys = keyof typeof Properties
 
-export const Schema: Record<keyof typeof Properties, JSONSchema> = {
+export const Schema: Record<SchemaKeys, JSONSchema> = {
   [Properties.containerRuntime]: {
     type: "string",
     enum: Object.keys(ContainerRuntimes),
+  },
+  [Properties.defaults]: {
+    type: "object",
+    properties: {
+      entrypoint: {
+        type: "string",
+      },
+
+      environment: {
+        type: "object", // string => string
+        additionalProperties: {
+          type: "string",
+        },
+      },
+      interactive: {
+        type: "string",
+      },
+      port: {
+        type: "object", // number => number
+        additionalProperties: {
+          type: "number",
+        },
+      },
+      tty: {
+        type: "string",
+      },
+      volume: {
+        type: "object", // string => string
+        additionalProperties: {
+          type: "string",
+        },
+      },
+      workingDirectory: { type: "string" },
+    },
   },
 }
