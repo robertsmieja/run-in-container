c763017e177d16d98dd26cb45ef41f39ef40cc53
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-09-12 02:40:59 -0400

    Fix build by adding missing arg to initHook

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -2,7 +2,7 @@ import { Hook } from "@oclif/config"
 import { initConfig } from "../configuration"
 
 const initHook: Hook.Init = async () => {
-  await initConfig()
+  await initConfig({ rerun: false })
 }
 
 export default initHook
